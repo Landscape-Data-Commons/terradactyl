7748aa4efc99e145888b0b8d089a64c9ba5c217f
nstauffer <ngstauffer@gmail.com>
2018-06-20 14:15:18 -0700

    Undo addition of ensyms()

--- R/height.R
+++ R/height.R
@@ -3,7 +3,7 @@
 #' @param omit.zero Logical. If \code{TRUE} the results omit height measurements of \code {0}. Defaults to \code{FALSE}.
 #' @param method Character string. Indicates the type of indicator, \code{"max"}, which yields the average maximum height (of the herbaceous or woody heights) on the plot or \code{"mean"} which yields the mean height by functional group (woody/herbaceous).
 #' @param by.line Logical. If \code{TRUE} then the results will be calculated on a per-line basis. If \code{FALSE} then the results will be calculated on a per-plot basis. Defaults to \code{FALSE}.
-#' @param ... Optional character strings. One or more variable name from \code{lpi.tall} to calculate percent cover for, e.g. \code{"GrowthHabitSub"} to calculate percent cover by growth habits or \code{"GrowthHabitSub", "Duration"} to calculate percent cover for categories like perennial forbs, annual graminoids, etc.
+#' @param ... Optional bare variable names. One or more variable name from \code{lpi.tall} to calculate percent cover for, e.g. \code{GrowthHabitSub} to calculate percent cover by growth habits or \code{GrowthHabitSub, Duration} to calculate percent cover for categories like perennial forbs, annual graminoids, etc.
 #' @export mean.height
 
 mean.height <- function(lpi.height.tall,
@@ -12,11 +12,7 @@ mean.height <- function(lpi.height.tall,
                       by.line = FALSE,
                       ...){
   ## Get a list of the variables the user wants to group by.
-  grouping.variables <- rlang::quos(rlang::ensyms(...))
-
-  if (!isFALSE(!all(list(...) %in% names(lpi.height.tall)))){
-    stop("All grouping variables need to be variables in the lpi.tall data frame.")
-  }
+  grouping.variables <- rlang::quos(...)
 
   if (class(lpi.height.tall) != "data.frame"){
     stop("lpi.height.tall must be a data frame.")
