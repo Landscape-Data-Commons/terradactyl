fd109d2e1b0d402849bebfe7419fef2c2319e9e1
smccord <sarah.e.mccord@gmail.com>
2018-06-28 13:09:39 -0600

    fix if/else DIMAKey

--- R/gather.height.R
+++ R/gather.height.R
@@ -30,8 +30,8 @@ gather.height <- function(dsn,
   lpi.detail <- suppressWarnings(sf::st_read(dsn=dsn, layer = "tblLPIDetail"))
   lpi.header <- suppressWarnings(sf::st_read(dsn=dsn, layer = "tblLPIHeader"))
 
-  ## TODO: Make this an else statement
-  if(colnames(lpi.header) %in% "DIMAKey"){
+  ## Make this an else statement
+  if(any(colnames(lpi.header) %in% "DIMAKey")){
     levels <- rlang::quos(PrimaryKey, DIMAKey)
   } else {
     levels <- rlang::quos(PrimaryKey)
