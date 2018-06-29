76aa4f0fad858a2aa1508822dcd5b4b7fe96b13d
smccord <sarah.e.mccord@gmail.com>
2018-06-28 20:48:38 -0600

    add tall parameter to function

--- R/height.R
+++ R/height.R
@@ -1,15 +1,17 @@
 #' Calculate the vegetation height
-#' @param lpi.tall A tall/long-format data frame. Use the data frame \code{"height"} from the \code{gather.lpi()} output.
+#' @param height.tall A tall/long-format data frame. Use the data frame \code{"height"} from the \code{gather.lpi()} output.
 #' @param omit.zero Logical. If \code{TRUE} the results omit height measurements of \code{0}. Defaults to \code{FALSE}.
 #' @param method Character string. Indicates the type of indicator, \code{"max"}, which yields the average maximum height (of the herbaceous or woody heights) on the plot or \code{"mean"} which yields the mean height by functional group (woody/herbaceous).
 #' @param by.line Logical. If \code{TRUE} then the results will be calculated on a per-line basis. If \code{FALSE} then the results will be calculated on a per-plot basis. Defaults to \code{FALSE}.
 #' @param ... Optional bare variable names. One or more variable name from \code{lpi.tall} to calculate percent cover for, e.g. \code{GrowthHabitSub} to calculate percent cover by growth habits or \code{GrowthHabitSub, Duration} to calculate percent cover for categories like perennial forbs, annual graminoids, etc.
+#' @param tall Logical. If \code{TRUE} then the returned data frame will be tall rather than wide and will not have observations for non-existent values e.g., if no data fell into a group on a plot, there will be no row for that group on that plot. Defaults to \code{FALSE}.
 #' @export mean.height
 
 mean.height <- function(lpi.height.tall,
                       method = "mean",
                       omit.zero = FALSE,
                       by.line = FALSE,
+                      tall=FALSE,
                       ...){
   ## Get a list of the variables the user wants to group by.
   grouping.variables <- rlang::quos(...)
@@ -56,7 +58,10 @@ mean.height <- function(lpi.height.tall,
       dplyr::group_by(!!!level, !!!grouping.variables) %>%
       dplyr::summarize(max.height = mean(max))
   }
-
+#Convert to wide format
+  if(tall){
+    summary<-summary %>% tidyr::spread(key=indicator, value=mean.height)
+  }
 
   return(summary)
 }
