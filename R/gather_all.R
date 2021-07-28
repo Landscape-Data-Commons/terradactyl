218351f6b2e8b8246cce46bea9d83d7b585bbb3f
josephbrehm <joseph.brehm@gmail.com>
2021-07-28 14:36:49 -0400

    add verbose to gather all

--- R/gather_all.R
+++ R/gather_all.R
@@ -12,6 +12,8 @@
 #' must be named as expected by the individual gather_functions.
 #' @param outfolder Character string. Name of a folder to save all output to.
 #' If the specified folder does not exist, the function will create it.
+#' @param verbose True/False. When true, displays progress information, and 
+#' reports missing input data. 
 #' @importFrom magrittr %>%
 #' @name gather_all
 #' @family <gather>
@@ -26,7 +28,7 @@
 #' })
 #' gather_all(dflist = all_data, outfolder = "output")
 
-gather_all <- function(dsn = NULL, dflist = NULL, outfolder) {
+gather_all <- function(dsn = NULL, dflist = NULL, outfolder, verbose = T) {
   # prep ####
   # most people wouldnt put the trailing f-slash on a folder name so add it in
   if(substr(outfolder, nchar(outfolder), nchar(outfolder)) != "/") {
@@ -38,7 +40,7 @@ gather_all <- function(dsn = NULL, dflist = NULL, outfolder) {
   # if both dsn and dflist are provided, drop dsn
   if(!is.null(dflist) & !is.null(dsn)){
     dsn <- NULL
-    print("Both dsn and dflist were provided. Dsn will be ignored")
+    if(verbose) print("Both dsn and dflist were provided. Dsn will be ignored")
   }
   
   # extract names, check against these before trying to load data
@@ -81,219 +83,233 @@ gather_all <- function(dsn = NULL, dflist = NULL, outfolder) {
   GPS <- dflist[["GPS"]]
   
   # Gap ####
-  print("Preparing Gap data")
   if(("tblGapDetail" %in% names_rda & "tblGapHeader" %in% names_rda) |
      ("tblGapDetail" %in% names_gdb & "tblGapHeader" %in% names_gdb)){
+    if(verbose) print("Gathering AIM gap")
     gap_aim <- gather_gap(dsn = dsn, source = "AIM", 
                           tblGapDetail = tblGapDetail,
                           tblGapHeader = tblGapHeader)
   } else {
     gap_aim <- NULL
-    print("tblGapDetail and/or tblGapHeader not found. AIM gap will not be prepared.")
+    if(verbose) print("tblGapDetail and/or tblGapHeader not found. Skipping AIM Gap.")
   }
   
   if(("GINTERCEPT" %in% names_rda & "POINT" %in% names_rda) |
      ("GINTERCEPT" %in% names_gdb & "POINT" %in% names_gdb)){
+    if(verbose) print("Gathering LMF gap")
     gap_lmf <- gather_gap(dsn = dsn, source = "LMF", 
                           GINTERCEPT = GINTERCEPT, 
                           POINT = POINT)
   } else {
     gap_lmf <- NULL
-    print("GINTERCEPT and/or POINT not found. LMF gap will not be prepared.")
+    if(verbose) print("GINTERCEPT and/or POINT not found. Skipping LMF Gap.")
   }
   
   gap_tall <- dplyr::bind_rows(gap_aim, gap_lmf)
-  saveRDS(gap_tall,
-          file = paste(outfolder, "gap_tall.Rdata", sep = ""))
+  if(1 <= nrow(gap_tall)){
+    write.csv(gap_tall,
+              file = paste(outfolder, "gap_tall.csv", sep = ""), row.names = F)
+  }
   rm(gap_aim, gap_lmf)
   invisible(gc())
   
   # Soil stability ####
-  print("Preparing Soil Stability data")
-  
   if(("tblSoilStabDetail" %in% names_rda & "tblSoilStabHeader" %in% names_rda) |
      ("tblSoilStabDetail" %in% names_gdb & "tblSoilStabHeader" %in% names_gdb)){
+    if(verbose) print("Gathering AIM soil stability")
     soilstab_aim <- gather_soil_stability(dsn = dsn, source = "AIM",
                                           tblSoilStabDetail = tblSoilStabDetail,
                                           tblSoilStabHeader = tblSoilStabHeader)
   } else {
     soilstab_aim <- NULL
-    print("tblSoilStabDetail and/or tblSoilStabHeader not found. AIM Soil Stability will not be prepared.")
+    if(verbose) print("tblSoilStabDetail and/or tblSoilStabHeader not found. Skipping AIM Soil Stability.")
   }
   
   if(("SOILDISAG" %in% names_rda) |
      ("SOILDISAG" %in% names_gdb)){
+    if(verbose) print("Gathering LMF soil stability")
     soilstab_lmf <- gather_soil_stability(dsn = dsn, source = "LMF",
                                           SOILDISAG = SOILDISAG)
   } else {
     soilstab_lmf <- NULL
-    print("SOILDISAG not found. LMF Soil Stability will not be prepared.")
+    if(verbose) print("SOILDISAG not found. Skipping LMF Soil Stability.")
   }
   
   soilstab_tall <- dplyr::bind_rows(soilstab_aim, soilstab_lmf)
-  saveRDS(soilstab_tall,
-          file = paste(outfolder, "soilstab_tall.Rdata", sep = "")
-  )
+  if(1 <= nrow(soilstab_tall)){
+    write.csv(soilstab_tall,
+              file = paste0(outfolder, "soilstab_tall.csv"), row.names = F)
+  }
   rm(soilstab_aim, soilstab_lmf)
   invisible(gc())
   
   # LPI ####
-  print("Preparing LPI data")
-  
   if(("tblLPIDetail" %in% names_rda & "tblLPIHeader" %in% names_rda) |
      ("tblLPIDetail" %in% names_gdb & "tblLPIHeader" %in% names_gdb)){
+    if(verbose) print("Gathering AIM LPI")
     lpi_aim <- gather_lpi(dsn = dsn, file_type = "gdb", source = "AIM",
                           tblLPIDetail = tblLPIDetail, tblLPIHeader = tblLPIHeader)
   } else {
     lpi_aim <- NULL
-    print("tblLPIDetail and/or tblLPIHeader not found. AIM LPI will not be prepared.")
+    if(verbose) print("tblLPIDetail and/or tblLPIHeader not found. Skipping AIM LPI.")
   }
   
   if(("PINTERCEPT" %in% names_rda) |
      ("PINTERCEPT" %in% names_gdb)){
+    if(verbose) print("Gathering LMF LPI")
     lpi_lmf <- gather_lpi(dsn = dsn, file_type = "gdb", source = "LMF", 
                           PINTERCEPT = PINTERCEPT)
   } else {
     lpi_lmf <- NULL
-    print("PINTERCEPT not found. LMF LPI will not be prepared.")
+    if(verbose) print("PINTERCEPT not found. Skipping LMF LPI.")
   }
   
   lpi_tall <- dplyr::bind_rows(lpi_aim, lpi_lmf)
-  saveRDS(lpi_tall,
-          file = paste(outfolder, "lpi_tall.Rdata", sep = "")
-  )
+  if(1 <= nrow(lpi_tall)){
+    write.csv(lpi_tall,
+              file = paste(outfolder, "lpi_tall.csv", sep = ""), row.names = F
+    )
+  }
   rm(lpi_aim, lpi_lmf)
   invisible(gc())
   
   # Height ####
-  print("Preparing Vegetation Height data")
   if(("tblLPIDetail" %in% names_rda & "tblLPIHeader" %in% names_rda) |
      ("tblLPIDetail" %in% names_gdb & "tblLPIHeader" %in% names_gdb)){
+    if(verbose) print("Gathering AIM Height")
     height_aim <- gather_height(dsn = dsn, file_type = "gdb", source = "AIM",
                                 tblLPIDetail = tblLPIDetail, tblLPIHeader = tblLPIHeader)
   } else {
     height_aim <- NULL
-    print("tblLPIDetail and/or tblLPIHeader not found. AIM Height will not be prepared.")
+    if(verbose) print("tblLPIDetail and/or tblLPIHeader not found. Skipping AIM Height.")
   }
   
   if(("PASTUREHEIGHTS" %in% names_rda) |
      ("PASTUREHEIGHTS" %in% names_gdb)){
-    
+    if(verbose) print("Gathering LMF Height")
     height_lmf <- gather_height(dsn = dsn, file_type = "gdb", source = "LMF",
                                 PASTUREHEIGHTS = PASTUREHEIGHTS)
   } else {
     height_lmf <- NULL
-    print("PASTUREHEIGHTS not found. LMF Height will not be prepared.")
+    if(verbose) print("PASTUREHEIGHTS not found. Skipping LMF Height.")
   }
   
-  
   height_tall <- dplyr::bind_rows(height_aim, height_lmf)
-  saveRDS(height_tall,
-          file = paste(outfolder, "height_tall.Rdata", sep = ""))
+  if(1 <= nrow(height_tall)){
+    write.csv(height_tall,
+              file = paste(outfolder, "height_tall.csv", sep = ""), row.names = F)
+  }
   rm(height_lmf, height_aim)
   invisible(gc())
   
   # Species inventory ####
-  print("Preparing Species Inventory data")
   if(("tblSpecRichDetail" %in% names_rda & "tblSpecRichHeader" %in% names_rda) |
      ("tblSpecRichDetail" %in% names_gdb & "tblSpecRichHeader" %in% names_gdb)){
+    if(verbose) print("Gathering AIM species inventory")
     spp_inventory_aim <- gather_species_inventory(dsn = dsn, source = "AIM", 
                                                   tblSpecRichDetail = tblSpecRichDetail,
                                                   tblSpecRichHeader = tblSpecRichHeader)
     
   } else {
     spp_inventory_aim <- NULL
-    print("tblSpecRichDetail and/or tblSpecRichHeader not found. AIM Species Inventory will not be prepared.")
+    if(verbose) print("tblSpecRichDetail and/or tblSpecRichHeader not found. Skipping AIM Species Inventory.")
   }
   if(("PLANTCENSUS" %in% names_rda) |
      ("PLANTCENSUS" %in% names_gdb)){
-    
+    if(verbose) print("Gathering LMF species inventory")
     spp_inventory_lmf <- gather_species_inventory(dsn = dsn, source = "LMF", 
                                                   PLANTCENSUS = PLANTCENSUS,
                                                   file_type = "gdb")
   } else {
     spp_inventory_lmf <- NULL
-    print("PLANTCENSUS not found. LMF Species Inventory will not be prepared.")
+    if(verbose) print("PLANTCENSUS not found. Skipping LMF Species Inventory.")
   }
   
   spp_inventory_tall <- dplyr::bind_rows(spp_inventory_aim, spp_inventory_lmf)
-  saveRDS(spp_inventory_tall,
-          file = paste(outfolder, "spp_inventory_tall.Rdata", sep = ""))
+  if(1 <= nrow(spp_inventory_tall)){
+    write.csv(spp_inventory_tall,
+              file = paste(outfolder, "spp_inventory_tall.csv", sep = ""), row.names = F)
+  }
   rm(spp_inventory_aim, spp_inventory_lmf)
   invisible(gc())
   
   # soil horizons ####
-  print("Preparing Soil Horizon data")
-  
   if(("tblSoilPitHorizons" %in% names_rda) |
      ("tblSoilPitHorizons" %in% names_gdb)){
-    
+    if(verbose) print("Gathering AIM soil horizon data")
     hz_aim <- gather_soil_horizon(dsn = dsn, source = "AIM",
                                   tblSoilPitHorizons = tblSoilPitHorizons)
   } else {
     hz_aim <- NULL
-    print("tblSoilPitHorizons not found. AIM Horizons will not be prepared.")
+    if(verbose) print("tblSoilPitHorizons not found. Skipping AIM Horizons.")
   }
   if(("SOILHORIZON" %in% names_rda) |
      ("SOILHORIZON" %in% names_gdb)){
-    
+    if(verbose) print("Gathering LMF soil horizon data")
     hz_lmf <- gather_soil_horizon(dsn = dsn, source = "LMF", SOILHORIZON = SOILHORIZON)
   } else {
     hz_lmf <- NULL
-    print("SOILHORIZON not found. LMF Horizons will not be prepared.")
+    if(verbose) print("SOILHORIZON not found. Skipping LMF Horizons.")
   }
   hz_tall <- dplyr::bind_rows(hz_aim, hz_lmf)
-  saveRDS(hz_tall, file = paste0(outfolder, "soil_horizons_tall.rdata"))
+  if(1 <= nrow(hz_tall)){
+    write.csv(hz_tall, file = paste0(outfolder, "soil_horizons_tall.csv"), row.names = F)
+  }
   rm(hz_aim, hz_lmf)
   invisible(gc())
   
   # soil summary ####
-  print("Preparing Soil Pit Summary data")
   if(("tblSoilPitHorizons" %in% names_rda & "tblSoilPits" %in% names_rda) |
      ("tblSoilPitHorizons" %in% names_gdb & "tblSoilPits" %in% names_gdb)){
+    if(verbose) print("Gathering AIM soil summary")
     pit_aim <- gather_soil_summary(dsn = dsn, source = "AIM", 
                                    tblSoilPitHorizons = tblSoilPitHorizons,
                                    tblSoilPits = tblSoilPits)
   } else {
     pit_aim <- NULL
-    print("tblSoilPitHorizons and/or tblSoilPits not found. AIM Pit Summary will not be prepared.")
+    if(verbose) print("tblSoilPitHorizons and/or tblSoilPits not found. Skipping AIM Soil Summary.")
   }
   if(("SOILHORIZON" %in% names_rda) |
      ("SOILHORIZON" %in% names_gdb)){
+    if(verbose) print("Gathering LMF soil summary")
     pit_lmf <- gather_soil_summary(dsn = dsn, source = "LMF", SOILHORIZON = SOILHORIZON)
   } else {
     pit_lmf <- NULL
-    print("SOILHORIZON not found. LMF Pit Summary will not be prepared.")
+    if(verbose) print("SOILHORIZON not found. Skipping LMF soil Summary.")
   }
   pit_tall <- dplyr::bind_rows(pit_aim, pit_lmf)
-  saveRDS(pit_tall, file = paste0(outfolder, "soil_summary_tall.Rdata"))
+  if(1 <= nrow(pit_tall)){
+    write.csv(pit_tall, file = paste0(outfolder, "soil_summary_tall.csv"), row.names = F)
+  }
+  
   rm(pit_aim, pit_lmf)
   invisible(gc())
   
   # iirh ####
-  print("Preparing Rangeland Health data")
   if(("tblQualDetail" %in% names_rda & "tblQualHeader" %in% names_rda) |
      ("tblQualDetail" %in% names_gdb & "tblQualHeader" %in% names_gdb)){
-    
+    if(verbose) print("Gathering AIM IIRH data")
     iirh_aim <- gather_rangeland_health(dsn = dsn, source = "AIM",
                                         tblQualDetail = tblQualDetail,
                                         tblQualHeader = tblQualHeader)
   } else {
     iirh_aim <- NULL
-    print("tblQualDetail and/or tblQualHeader not found. AIM Rangeland Health will not be prepared.")
+    if(verbose) print("tblQualDetail and/or tblQualHeader not found. Skipping AIM Rangeland Health.")
   }
   if(("RANGEHEALTH" %in% names_rda) |
      ("RANGEHEALTH" %in% names_gdb)){
-    
+    if(verbose) print("Gathering LMF IIRH data")
     iirh_lmf <- gather_rangeland_health(dsn = dsn, source = "LMF",
                                         RANGEHEALTH = RANGEHEALTH)
     
   } else {
     iirh_lmf <- NULL
-    print("RANGEHEALTH not found. LMF Rangeland Health will not be prepared.")
+    if(verbose) print("RANGEHEALTH not found. Skipping LMF Rangeland Health.")
   }
   iirh_tall <- dplyr::bind_rows(iirh_aim, iirh_lmf)
-  saveRDS(iirh_tall, file = paste0(outfolder, "rangeland_health_tall.Rdata"))
+  if(1 <= nrow(iirh_tall)){
+    write.csv(iirh_tall, file = paste0(outfolder, "rangeland_health_tall.csv"), row.names = F)
+  }
   rm(iirh_aim, iirh_lmf)
   invisible(gc())
   
@@ -301,26 +317,25 @@ gather_all <- function(dsn = NULL, dflist = NULL, outfolder) {
   # header_aim <- gather_header(dsn = dsn, source = "AIM")
   # header_lmf <- gather_header(dsn = dsn, source = "LMF")
   # header <- dplyr::bind_rows(header_aim, header_lmf)
-  # saveRDS(header,
+  # write.csv(header,
   #         file = paste(outfolder, "header.Rdata", sep = "")
   # )
   # 
   # plot characterization ####
-  print("Preparing Plot Characterization data")
   if(("tblPlots" %in% names_rda) |
      ("tblPlots" %in% names_gdb)){
-    
+    if(verbose) print("Gathering AIM plot characterization")
     plotchar_aim <- gather_plot_characterization(dsn = dsn,
                                                  source = "AIM",
                                                  tblPlots = tblPlots)
     
   } else {
     plotchar_aim <- NULL
-    print("tblPlots not found. AIM Plot Characterization will not be prepared.")
+    if(verbose) print("tblPlots not found. Skipping AIM Plot Characterization.")
   }
   if(("POINT" %in% names_rda & "POINTCOORDINATES" %in% names_rda & "GPS" %in% names_rda) |
      ("POINT" %in% names_gdb & "POINTCOORDINATES" %in% names_gdb & "GPS" %in% names_gdb)){
-    
+    if(verbose) print("Gathering LMF plot characterization")
     plotchar_lmf <- gather_plot_characterization(dsn = dsn,
                                                  source = "LMF",
                                                  POINT = POINT,
@@ -328,15 +343,17 @@ gather_all <- function(dsn = NULL, dflist = NULL, outfolder) {
                                                  GPS = GPS)
   } else {
     plotchar_lmf <- NULL
-    print("POINT and/or GPS and/or POINTCOORDINATES not found. LMF Plot Characterization will not be prepared.")
+    if(verbose) print("POINT and/or GPS and/or POINTCOORDINATES not found. Skipping LMF Plot Characterization.")
   }
   plotchar_tall <- dplyr::bind_rows(plotchar_aim, plotchar_lmf)
-  saveRDS(plotchar_tall, file = paste0(outfolder, "plot_characterization.Rdata"))
+  if(1 <= nrow(plotchar_tall)){
+    write.csv(plotchar_tall, file = paste0(outfolder, "plot_characterization.csv"), row.names = F)
+  }
   rm(plotchar_aim, plotchar_lmf)
   invisible(gc())
   
   # output ####
-  
+
   list_out <- list(
     gap_tall, height_tall, hz_tall, lpi_tall, pit_tall, plotchar_tall, 
     soilstab_tall, spp_inventory_tall
@@ -346,5 +363,4 @@ gather_all <- function(dsn = NULL, dflist = NULL, outfolder) {
                        "PlotCharacterization", "SoilStability", "SpeciesInventory")
   
   return(list_out)
-  #####
 }
