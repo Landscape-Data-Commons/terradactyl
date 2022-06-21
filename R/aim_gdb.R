f822e9e6d7a04338492f3d43e71f20b6be49179d
josephbrehm <64801701+josephbrehm@users.noreply.github.com>
2022-06-21 14:04:03 -0600

    emergency fix: gather_header
    gather header requried tblSpecRichHeader, but did not accept it as an input

--- R/aim_gdb.R
+++ R/aim_gdb.R
@@ -15,6 +15,7 @@ gather_header_terradat <- function(dsn = NULL, tblPlots = NULL, tblLPIHeader = N
 
   # tblPlots provides the link between species tables
   if(!is.null(tblPlots)){
+  if(!is.null(tblPlots) & !is.null(tblLPIHeader) & !is.null(tblSpecRichHeader) & !is.null(tblGapHeader)){
     header <- tblPlots
   } else if (!is.null(dsn)){
     # (LPI, Height, Species Richness) and tblStateSpecies
