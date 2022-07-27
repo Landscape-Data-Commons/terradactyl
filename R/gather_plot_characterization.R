4cf965dd46d710c4ed95371da67ed10140d8f917
josephbrehm <64801701+josephbrehm@users.noreply.github.com>
2022-07-27 09:04:12 -0600

    Update gather_plot_characterization.R
    lmf:
    drop countyno and state no
    add mlra and ecolsite
    
    aim:
    add elevation, mlra

--- R/gather_plot_characterization.R
+++ R/gather_plot_characterization.R
@@ -1,53 +1,53 @@
 #' Convert plot data into a tall, tidy data frame
 #'
-#' @description Given wide format plot data, create a tall format data frame 
+#' @description Given wide format plot data, create a tall format data frame
 #' usable by other terradactyl functions.
-#' @param dsn Character string. The full filepath and filename (including file 
-#' extension) of the geodatabase or text file containing the table of interest. 
-#' This field is unnecessary if you provide either tblPlots (AIM/DIMA/TerrADat) 
+#' @param dsn Character string. The full filepath and filename (including file
+#' extension) of the geodatabase or text file containing the table of interest.
+#' This field is unnecessary if you provide either tblPlots (AIM/DIMA/TerrADat)
 #' or POINT (LMF/NRI).
-#' @param source Character string. The data source format, 
+#' @param source Character string. The data source format,
 #' \code{"AIM", "TerrADat", "DIMA", "LMF", "NRI"} (case independent).
-#' @param tblPlots Dataframe of the data structure tblPlots from the 
+#' @param tblPlots Dataframe of the data structure tblPlots from the
 #' DIMA database with the addition of PrimaryKey and DBKey fields. Use when data
 #' source is AIM, DIMA, or TerrADat; alternately provide dsn.
-#' @param POINT Dataframe of the data structure PINTERCEPT from the LMF/NRI 
-#' database with the addition of PrimaryKey and DBKey fields. Use when source 
+#' @param POINT Dataframe of the data structure PINTERCEPT from the LMF/NRI
+#' database with the addition of PrimaryKey and DBKey fields. Use when source
 #' is LMF or NRI; alternately provide dsn.
-#' @param POINTCOORDINATES Dataframe of the data structure POINTCOORDINATES from the LMF/NRI 
-#' database with the addition of PrimaryKey and DBKey fields. Use when source 
+#' @param POINTCOORDINATES Dataframe of the data structure POINTCOORDINATES from the LMF/NRI
+#' database with the addition of PrimaryKey and DBKey fields. Use when source
 #' is LMF or NRI; alternately provide dsn.
-#' @param GPS Dataframe of the data structure GPS from the LMF/NRI 
-#' database with the addition of PrimaryKey and DBKey fields. Use when source 
+#' @param GPS Dataframe of the data structure GPS from the LMF/NRI
+#' database with the addition of PrimaryKey and DBKey fields. Use when source
 #' is LMF or NRI; alternately provide dsn.
-#' #' @param file_type Character string that denotes the source file type of the 
-#' LMF/NRI data, \code{"gdb"} or \code{"txt"}. Not necessary for 
+#' #' @param file_type Character string that denotes the source file type of the
+#' LMF/NRI data, \code{"gdb"} or \code{"txt"}. Not necessary for
 #' AIM/DIMA/TerrADat, or if POINT, POINTCOORDINATES, and GPS are provided.
 #' @importFrom magrittr %>%
 #' @name gather_plot_characterization
 #' @family <gather>
 #' @return A tall data frame containing plot characterization data
 #' @examples
-#' gather_plot_characterization(dsn = "Path/To/AIM_Geodatabase.gdb", 
+#' gather_plot_characterization(dsn = "Path/To/AIM_Geodatabase.gdb",
 #'                              source = "AIM")
-#' gather_plot_characterization(dsn = "Path/To/LMF_Geodatabase.gdb", 
+#' gather_plot_characterization(dsn = "Path/To/LMF_Geodatabase.gdb",
 #'                              source = "LMF")
-#' 
+#'
 #' aim_plots <- read.csv("Path/To/tblPlots.csv")
-#' gather_plot_characterization(source = "AIM", 
+#' gather_plot_characterization(source = "AIM",
 #'                              tblPlots = aim_plots)
-#' 
+#'
 #' lmf_pintercept <- read.csv("Path/To/PINTERCEPT.csv")
 #' lmf_pointcoords <- read.csv("Path/To/POINTCOORDINATES.csv")
 #' lmf_gps <- read.csv("Path/To/GPS.csv")
-#' gather_plot_characterization(source = "LMF", 
+#' gather_plot_characterization(source = "LMF",
 #'                              PINTERCEPT = lmf_pintercept,
 #'                              POINTCOORDINATES = lmf_pointcoords,
 #'                              GPS = lmf_gps)
 
 #' @export gather_plot_characterization_terradat
 #' @rdname gather_plot_characterization
-gather_plot_characterization_terradat <- function(dsn = NULL, 
+gather_plot_characterization_terradat <- function(dsn = NULL,
                                                   tblPlots = NULL){
   if(!is.null(tblPlots)){
     plot_raw <- tblPlots
@@ -59,16 +59,16 @@ gather_plot_characterization_terradat <- function(dsn = NULL,
   plot_tall <- plot_raw %>%
     dplyr::select_if(names(.) %in% c(
       'PrimaryKey', 'DBKey', 'ProjectKey',
-      'SpeciesState', 
-      'Latitude', 'Longitude', 'Zone', 'Datum',
+      'SpeciesState',
+      'Latitude', 'Longitude',
       'State', 'County',
-      'EcolSite', 'ParentMaterial', 'Slope', 'Aspect', 'ESD_SlopeShape',
-      'LandscapeType', 'LandscapeTypeSecondary', 'HillslopeType', 
+      'EcolSite', 'ParentMaterial', 'Slope', 'Elevation', 'Aspect', 'ESD_SlopeShape',
+      'LandscapeType', 'LandscapeTypeSecondary', 'HillslopeType',
       'ESD_Series',
       'Observer', 'Recorder',
       'EstablishDate',
       'ESD_Investigators'
-    )) %>% 
+    )) %>%
     dplyr::rename(
       Latitude_NAD83 = Latitude,
       Longitude_NAD83 = Longitude,
@@ -89,84 +89,104 @@ gather_plot_characterization_terradat <- function(dsn = NULL,
       Aspect = suppressWarnings(as.numeric(Aspect)),
       Slope = suppressWarnings(as.numeric(Slope)),
       Latitude_NAD83 = suppressWarnings(as.numeric(Latitude_NAD83)),
-      Longitude_NAD83 = suppressWarnings(as.numeric(Longitude_NAD83))) %>% 
+      Longitude_NAD83 = suppressWarnings(as.numeric(Longitude_NAD83)),
+      EcolSite = terradactyl::ecosite_qc(EcolSite),
+      MLRA = substr(EcolSite, 2, 5) %>% gsub("NKNO", NA, .)) %>%
     dplyr::select(-SlopeShape)
-  
+
   return(plot_tall)
 }
 
 
 #' @export gather_plot_characterization_lmf
 #' @rdname gather_plot_characterization
-gather_plot_characterization_lmf <-   function(dsn = NULL, 
-                                               POINT = NULL,  
+gather_plot_characterization_lmf <-   function(dsn = NULL,
+                                               POINT = NULL,
                                                POINTCOORDINATES = NULL,
                                                GPS = NULL,
+                                               ESFSG = NULL,
                                                file_type = NULL
 ) {
   ### input ####
-  ## update to have coord and gps
-  if (!is.null(POINT) & !is.null(POINTCOORDINATES) & !is.null(GPS)){
+  if (!is.null(POINT) & !is.null(POINTCOORDINATES) & !is.null(GPS) & !is.null(ESFSG)){
     point_lmf_raw <- POINT
     coord_lmf_raw <- POINTCOORDINATES
     gps_lmf_raw   <- GPS
+    esfsg_lmf_raw <- ESFSG
   } else if(!is.null(dsn)){
     point_lmf_raw <-
       sf::st_read(dsn = dsn, layer = "POINT", stringsAsFactors = FALSE, quiet = T)
-    
+
     coord_lmf_raw <-
       sf::st_read(dsn = dsn, layer = "POINTCOORDINATES", stringsAsFactors = FALSE, quiet = T)
-    
+
     gps_lmf_raw <-
       sf::st_read(dsn = dsn, layer = "GPS", stringsAsFactors = FALSE, quiet = T)
-    
+
+    esfsg_lmf_raw <-
+      sf::st_read(dsn = dsn, layer = "ESFSG", stringsAsFactors = FALSE, quiet = T)
+
+
   } else{
-    stop("Supply either POINT, POINTCOORDINATES, and GPS, or the path to a GDB containing those tables")
-  }  
-  
+    stop("Supply either POINT, POINTCOORDINATES, ESFSG, and GPS, or the path to a GDB containing those tables")
+  }
+
   # get slope shape from POINT
   point_lmf <- point_lmf_raw %>% dplyr::select(
-    DBKey, PrimaryKey, 
-    CountyNo = COUNTY, StateNo = STATE,
-    SlopeShapeVertical = VERTICAL_SLOPE_SHAPE, 
+    DBKey, PrimaryKey,
+    SlopeShapeVertical = VERTICAL_SLOPE_SHAPE,
     SlopeShapeHorizontal = HORIZONTAL_SLOPE_SHAPE,
     Slope = SLOPE_PERCENT, Aspect = SLOPE_ASPECT
   ) %>% dplyr::mutate(
     # reclass aspect into degrees
     Aspect = suppressWarnings(as.numeric(dplyr::recode(Aspect,
-                                                       "N" = "0", 
-                                                       "NE" = "45", 
-                                                       "E" = "90", 
-                                                       "SE" = "135", 
-                                                       "S" = "180", 
-                                                       "SW" = "225", 
+                                                       "N" = "0",
+                                                       "NE" = "45",
+                                                       "E" = "90",
+                                                       "SE" = "135",
+                                                       "S" = "180",
+                                                       "SW" = "225",
                                                        "W" = "270",
-                                                       "NW" = "315"))))
-  
+                                                       "NW" = "315")))
+    # get MLRA from ecological site id
+    )
+
   # get gis data from POINTCOORDINATES
   coord_lmf <- coord_lmf_raw %>% dplyr::select(
     PrimaryKey, DBKey,
     Latitude_NAD83 = REPORT_LATITUDE,
     Longitude_NAD83 = REPORT_LONGITUDE,
   )
-  
+
   # get gis from GPS
   gps_lmf <- gps_lmf_raw %>% dplyr::select(
     PrimaryKey, DBKey,
     Elevation = ELEVATION
   )
-  
+
+  # get ecological site and mlra from ESFSG
+  esfsg_lmf <- esfsg_lmf_raw %>% dplyr::mutate(
+    EcolSite = paste0(ESFSG_MLRA, ESFSG_SITE, ESFSG_STATE) %>% ecosite_qc(),
+    MLRA = ESFSG_MLRA %>% gsub("^$", NA, .)
+  ) %>% dplyr::select(
+    PrimaryKey, DBKey,
+    EcolSite, MLRA
+  )
+
   # join GIS
   gis_lmf  <- dplyr::full_join(coord_lmf, gps_lmf, by = c("PrimaryKey", "DBKey"))
-  
+
   # join gis and plot data
-  plot_lmf <- dplyr::left_join(point_lmf, gis_lmf, by = c("PrimaryKey", "DBKey"))
-  
+  plot_lmf <- dplyr::left_join(point_lmf, gis_lmf, by = c("PrimaryKey", "DBKey")) %>%
+    # and ecolsite data
+    dplyr::left_join(esfsg_lmf, by = c("PrimaryKey", "DBKey"))
+
+
   # last drop
-  plot_lmf <- plot_lmf %>% dplyr::select(
-    -c(Shape)
+  plot_lmf <- plot_lmf %>% dplyr::select_if(!names(.) %in% c(
+    "Shape", "StateNo", "CountyNo")
   )
-  
+
   return(plot_lmf)
 }
 
@@ -178,8 +198,9 @@ gather_plot_characterization <- function(dsn = NULL,
                                          POINT = NULL,
                                          POINTCOORDINATES = NULL,
                                          GPS = NULL,
+                                         ESFSG = NULL,
                                          file_type = "gdb"){
-  
+
   if(toupper(source) %in% c("AIM", "TERRADAT", "DIMA")){
     plotchar <- gather_plot_characterization_terradat(dsn = dsn,
                                                       tblPlots = tblPlots)
@@ -188,23 +209,24 @@ gather_plot_characterization <- function(dsn = NULL,
                                                  file_type = file_type,
                                                  POINT = POINT,
                                                  POINTCOORDINATES = POINTCOORDINATES,
-                                                 GPS = GPS)
+                                                 GPS = GPS,
+                                                 ESFSG = ESFSG)
   } else {
     stop("source must be AIM, TerrADat, DIMA, LMF, or NRI (all case independent)")
   }
-  
-  # plotchar$source <- toupper(source)  
-  plotchar$source <- source  
-  
+
+  # plotchar$source <- toupper(source)
+  plotchar$source <- source
+
   if("sf" %in% class(plotchar)) plotchar <- sf::st_drop_geometry(plotchar)
-  
+
   if (any(class(plotchar) %in% c("POSIXct", "POSIXt"))) {
-    change_vars <- names(plotchar)[do.call(rbind, vapply(plotchar, 
+    change_vars <- names(plotchar)[do.call(rbind, vapply(plotchar,
                                                          class))[, 1] %in% c("POSIXct", "POSIXt")]
-    plotchar <- dplyr::mutate_at(plotchar, dplyr::vars(change_vars), 
+    plotchar <- dplyr::mutate_at(plotchar, dplyr::vars(change_vars),
                                  dplyr::funs(as.character))
   }
-  
-  
+
+
   return(plotchar)
 }
