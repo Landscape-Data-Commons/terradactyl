768c6ff63d4d1365f0f49c0767cc2e1af802b349
bwheeler-nmsu <97681002+bwheeler-nmsu@users.noreply.github.com>
2026-03-19 15:43:30 -0600

    trblsht read nri

--- R/ingest.nri.R
+++ R/ingest.nri.R
@@ -193,7 +193,7 @@ read_nri_text <- function(path_parent,table_name, dsn, DBKey = "auto", GL_schema
         }
 
         if ("SURVEY" %in% colnames) {
-          UID <- read.csv(paste0(path_parent,"/sensitive_data/UID.csv"))
+          UID <- read.csv(paste0(path_parent,"sensitive_data/UID.csv"))
           data$PSU_POINT <- paste0(data$PSU, "_", data$POINT)
 
           data <- data %>%
