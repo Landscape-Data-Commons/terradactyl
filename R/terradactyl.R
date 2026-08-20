2b2f31d59588570e2dd1d4d268e3f04a5b8e06c0
Nelson Stauffer <nelson.stauffer@usda.gov>
2026-08-20 15:55:24 -0700

    Update package documentation with paper reference

--- R/terradactyl.R
+++ R/terradactyl.R
@@ -4,7 +4,10 @@
 #' data, calculating standard variables and indicators, and producing
 #' standard outputs for data repositories and models.
 #'
+#'
+#'
 #' @author Sarah McCord \email{sarah.mccord@usda.gov}
+#' @references \href{https://doi.org/10.1016/j.ecolind.2022.109511}{McCord, S.E., J.R. Brehm, S.H. Burnett, C. Dietrich, B. Edwards, M. Hernandez-Narvaez, E. Kachergis, L.J. Metz, F. Pierson, K.S. Ramirez, N.G. Stauffer, N.P. Webb, C.E. Tweedie. A framework and toolset for standardizing ecosystem indicators. Ecological Indicators, vol 144, 2022.}
 #' @docType package
 #' @name terradactyl
 #' @importFrom stats C complete.cases na.omit setNames
